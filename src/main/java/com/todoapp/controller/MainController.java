ae8436e7b40bfbcbf25590f7e1fc078bed31a8a1
King Pra <kingi.pra@gmail.com>
2019-06-23 21:28:52 -0500

    bug fix -index.html thymeleaf input field

--- src/main/java/com/todoapp/controller/MainController.java
+++ src/main/java/com/todoapp/controller/MainController.java
@@ -1,25 +1,20 @@
 package com.todoapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-//import com.derinsblog.blogpost.BlogPost;
 import com.todoapp.model.MainModel;
-import com.todoapp.model.Status;
 import com.todoapp.repo.MainRepo;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private MainRepo mainrepo;
-	
-	MainModel mainmodel = new MainModel();
+
 	@GetMapping("/")
 	public String getTask() {
 		return "index";
@@ -28,11 +23,12 @@ public class MainController {
 //	public Task newTaskObject() {
 //		return new Task(/);
 //	}
-	
-	@PostMapping("/")
-	public String sendTask(Model model,MainModel mainmodel) {
+
+	@PostMapping("/task")
+	public String sendTask(Model model, MainModel mainmodel) {
+		System.out.println(mainmodel);
 		mainrepo.save(new MainModel(mainmodel.getTask()));
-		model.addAttribute("mainmodel",mainmodel);
+		// model.addAttribute("mainmodel", mainmodel.getTask());
 		return "index";
 	}
 }
